c13575229b241af896e06f954ce548978bf9b84e
Marcelo Abrão da Silva <masilvasql@gmail.com>
2018-09-22 21:16:51 -0300

    componente display

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Button from './src/components/Button'
+import Display from './src/components/Display'
 export default class App extends React.Component {
+
+  state = {
+    displayValue: '0'
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
-          <Button label = 'AC'/>
-          <Button label = '/'/>
-          <Button label = '7'/>
-          <Button label = '8'/>
-          <Button label = '9'/>
-          <Button label = '*'/>
-          <Button label = '4'/>
-          <Button label = '5'/>
-          <Button label = '6'/>
-          <Button label = '-'/>
-          <Button label = '1'/>
-          <Button label = '2'/>
-          <Button label = '3'/>
-          <Button label = '+'/>
-          <Button label = '0'/>
-          <Button label = '.'/>
-          <Button label = '='/>
+          <Button label='AC' />
+          <Button label='/' />
+          <Button label='7' />
+          <Button label='8' />
+          <Button label='9' />
+          <Button label='*' />
+          <Button label='4' />
+          <Button label='5' />
+          <Button label='6' />
+          <Button label='-' />
+          <Button label='1' />
+          <Button label='2' />
+          <Button label='3' />
+          <Button label='+' />
+          <Button label='0' />
+          <Button label='.' />
+          <Button label='=' />
         </View>
-
       </View>
 
     );
@@ -36,10 +42,10 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight,
   },
-  buttons:{
+  buttons: {
     flexDirection: 'row',
     flexWrap: 'wrap',
-    
+
   }
 })
 
