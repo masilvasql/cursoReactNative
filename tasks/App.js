f6920a0b5b9d41937689b55fba2bfbd644606ce1
Marcelo Abrão da Silva <masilvasql@gmail.com>
2018-09-30 20:17:06 -0300

    componentes task

--- tasks/App.js
+++ tasks/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import commonStyles from './src/commonStyles'
+import Agenda from './src/screens/Agenda'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Open up App.js to start working on your app!</Text>
-      </View>
+      <Agenda/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcome:{
-    fontSize: 20,
-    textAlign:'center',
-    margin:10
-  }
-});
---
+++ tasks/src/screens/Agenda.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, ImageBackground,StatusBar, FlatList } from 'react-native'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import todayImage from '../../assets/imgs/today.jpg'
+import commonStyles from '../commonStyles'
+import Task from '../components/Task'
+
+export default class Agenda extends Component {
+
+    state = {
+        tasks:[
+            {id:Math.random(), desc:'Comprar curso', estimateAt:new Date(), doneAt:new Date()},
+            {id:Math.random(), desc:'Concluir o curso', estimateAt:new Date(), doneAt:null},
+            {id:Math.random(), desc:'Comprar curso', estimateAt:new Date(), doneAt:new Date()},
+            {id:Math.random(), desc:'Concluir o curso', estimateAt:new Date(), doneAt:null},
+            {id:Math.random(), desc:'Comprar curso', estimateAt:new Date(), doneAt:new Date()},
+            {id:Math.random(), desc:'Concluir o curso', estimateAt:new Date(), doneAt:null},
+            {id:Math.random(), desc:'Comprar curso', estimateAt:new Date(), doneAt:new Date()},
+            {id:Math.random(), desc:'Concluir o curso', estimateAt:new Date(), doneAt:null},
+            {id:Math.random(), desc:'Comprar curso', estimateAt:new Date(), doneAt:new Date()},
+            {id:Math.random(), desc:'Concluir o curso', estimateAt:new Date(), doneAt:null},
+            {id:Math.random(), desc:'Comprar curso', estimateAt:new Date(), doneAt:new Date()},
+            {id:Math.random(), desc:'Concluir o curso', estimateAt:new Date(), doneAt:null},
+        ]
+    }
+
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>
+                            {moment().locale('pt-br').format('ddd,D [de] MMMM')}
+                        </Text>
+                    </View>
+                </ImageBackground>
+                <View style ={styles.taskContainer}>
+                   <FlatList
+                        data ={this.state.tasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem = {({item})=> <Task {...item}/>}
+                   />
+
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        marginTop:StatusBar.currentHeight
+    },
+    background :{
+        flex:3,
+        
+    },
+    titleBar:{
+        flex:1,
+        justifyContent:'flex-end',
+    },
+    title:{
+        color:commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 10,
+    },
+    subtitle:{
+        color:commonStyles.colors.secondary,
+        fontSize:20,
+        marginLeft:20,
+        marginBottom:30
+    },
+    taskContainer:{
+        flex:7
+
+    }
+
+
+})
+
